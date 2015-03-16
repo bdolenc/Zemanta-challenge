5f17abb3c52034a06637402623dec8e1826ecf8a
dolenc <dolenc.blaz@gmail.com>
2015-03-16 22:53:57 +0100

    First commit.

---
+++ Clustering.py
@@ -0,0 +1,12 @@
+__author__ = 'blaz'
+
+import pandas as pd
+
+def main():
+    unemployment_file = "C:\BigData\Zemanta_challenge_1_data\unemployment_by_zip"
+
+
+
+def file_handler(file_name):
+    pd.read_csv(file_name)
+
