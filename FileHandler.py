b2045aae8354b178bb9a79bc31a2b39585e3fecd
dolenc <dolenc.blaz@gmail.com>
2015-04-09 12:40:20 +0200

    Added code to parse Total data.

--- FileHandler.py
+++ FileHandler.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import csv
 
 def file_handler(file1, file2, output_csv):
     """
@@ -18,17 +19,43 @@ def merge_establishments(input_name, output_name):
     Read chunks of establishments_by_zip in pandas
     dataframe and append to appropriate zip in dataframe
     """
-    df_establishments = pd.read_csv(input_name, delimiter='|')
-    df_merged = pd.DataFrame(data=np.zeros((0, 3)), columns=['ZIP', 'biz', '5 to 9'])
-    df_merged = df_merged.append({'ZIP':50234}, ignore_index='true')
-    df_merged.biz[df_merged.ZIP == 50234] = 5
+    #new column names
+    column_names = ['ZIP', '_all', '_1to4', '_5to9', '_10to19', '_20to49', '_50to99', '_100to249',
+                    '_250to499', '_500to999', '_1000plus']
+
+    #dataframe for optimized data
+    df_merged = pd.DataFrame(data=np.zeros((0, len(column_names))), columns=column_names)
+
+    #iterate through establishments_by_zip
+    with open(input_name, "rb") as csv_file:
+        data = csv.reader(csv_file, delimiter='|')
+        next(data, None) #skip header
+        #process each line and append to df_merged
+        first_zip = 10000
+        index = -1
+        for row in data:
+            #check if zip already in df
+            print row
+            if row[1] != first_zip:
+                index += 1
+                #add zip to df_merged
+                df_merged = df_merged.append({'ZIP': row[1]}, ignore_index='true')
+                #add all establishments number
+                df_merged.set_value(index, '_all', row[11])
+                first_zip = row[1]
+                col_i = 2
+            #add other data to a curr zip
+            else:
+                df_merged.set_value(index, column_names[col_i], row[11])
+                col_i += 1
+
+
+    #df_merged = df_merged.append({'ZIP': 50234}, ignore_index='true')
+    #df_merged._5to9[df_merged.ZIP == 50234] = 5
     print df_merged
 
 
     #print df_establishments
-
-
-
 
 unemployment_file = "C:\BigData\Zemanta_challenge_1_data/unemployment_by_zip.csv"
 population_file = "C:\BigData\Zemanta_challenge_1_data/population_densisty_area_by_zip.csv"
