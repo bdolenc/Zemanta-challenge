38a6da2963d8e7f021f240f2123ca377c927254d
dolenc <dolenc.blaz@gmail.com>
2015-05-19 20:59:59 +0200

    Logistic regression implemented.

--- StatisticalModelling.py
+++ StatisticalModelling.py
@@ -1,9 +1,11 @@
 from sklearn.ensemble import RandomForestClassifier as rfc
-from sklearn.cross_validation import StratifiedKFold as skf
+from sklearn import cross_validation
 from sklearn.metrics import roc_curve, auc
+from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import csv
+
 
 
 def prepare_data(learn_file, labels_file):
@@ -14,12 +16,16 @@ def prepare_data(learn_file, labels_file):
     """
     print "---preparing data...",
     l_set = pd.read_csv(learn_file, sep='\t')
+    #l_set = l_set.iloc[::3, :]
+
     #replace NaN values with zero.
     l_set = l_set.fillna(0)
+
     with open(labels_file, mode='r') as file_in:
         reader = csv.reader(file_in)
         c_labels = {float(rows[0]): rows[1] for rows in reader}
     #change ZIP with label
+    l_set['zip'] = l_set['zip'].convert_objects(convert_numeric=True).dropna()
     l_set['zip'] = l_set['zip'].map(c_labels.get)
 
     print "done---"
@@ -42,16 +48,55 @@ def prepare_data(learn_file, labels_file):
     return X, y
 
 
-def decide(X, y):
+def random_forest(X, y):
     #Scale data
     X = StandardScaler().fit_transform(X)
-    rfc.fit(X, y)
-    print rfc.predict_proba(X)
+    #split data to train and test
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.4)
+
+    #create rfc object
+    forest = rfc(n_estimators=100)
+    #fit training data
+    prob = forest.fit(X_train, y_train, ).predict_proba(X_test)
+    #compute ROC
+    fpr, tpr, thresholds = roc_curve(y_test, prob[:, 1])
+    roc_auc = auc(fpr, tpr)
+    #print fpr, tpr, thresholds
+    print "AUC Random Forest:  " + str(roc_auc)
+    #score on test part
+    #print forest.score(X_test, y_test)
     #fold =
+    #K Fold Cross Validation
+    """
+    cv = cross_validation.KFold(len(X), n_folds=5)
+    AUC = []
+    for train, test in cv:
+        prob = forest.fit(X[train], y[train]).predict_proba(X[test])
+        #compute ROC
+        fpr, tpr, thresholds = roc_curve(y[test], prob[:, 1])
+        AUC.append(auc(fpr, tpr))
+    print AUC
+    """
+
+def logistic_regression(X, y):
+    #Scale data
+    X = StandardScaler().fit_transform(X)
+    #split data to train and test
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.4)
+
+    log_r = LogisticRegression()
+    prob = log_r.fit(X_train, y_train).predict_proba(X_test)
+    #compute ROC
+    fpr, tpr, thresholds = roc_curve(y_test, prob[:, 1])
+    roc_auc = auc(fpr, tpr)
+    #print fpr, tpr, thresholds
+    print "AUC Logistic Regression:  " + str(roc_auc)
+
 
 
 learning_set = "C:\BigData\Zemanta_challenge_1_data/training_set.tsv"
-learning_part = "C:\BigData\Zemanta_challenge_1_data/training_part.tsv"
+learning_part = "C:\BigData\Zemanta_challenge_1_data/training_set.tsv"
 labels = "hc_results.csv"
 X, y = prepare_data(learning_part, labels)
-#decide(X, y)
+random_forest(X, y)
+logistic_regression(X, y)
